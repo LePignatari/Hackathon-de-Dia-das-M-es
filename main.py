3c808fe8d56f073495763364a075137463c7062a
Letícia <leticia.pignatari@gmail.com>
2026-05-17 23:54:01 -0300

    final hackathon version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from models.users import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
-app.config['SECRET_KEY'] = ('projeto-hackaton-sempre-perto')
+app.config['SECRET_KEY'] = 'projeto-hackathon-sempre-perto'
 db.init_app(app)
 
 from views import *
