8e6ba303a32b86515710e5848d97bfc1a32c41f7
Letícia <leticia.pignatari@gmail.com>
2026-05-18 00:36:58 -0300

    fix database deploy

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from db import db
 from models.users import User
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
 app.config['SECRET_KEY'] = 'projeto-hackathon-sempre-perto'
+
 db.init_app(app)
 
 from views import *
 
-if __name__ == "__main__":
-    with app.app_context():
+with app.app_context():
         db.create_all()
 
+if __name__ == "__main__":
     app.run(debug=True)
